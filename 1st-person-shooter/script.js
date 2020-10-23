34f6f9be4f4bcfb46936aadde9534b6e810e4b96
unknown <rjkaz@comcast.net>
2020-10-23 00:16:12 -0400

    update

--- 1st-person-shooter/script.js
+++ 1st-person-shooter/script.js
@@ -1,8 +1,9 @@
-var scene, camera, renderer, mesh;
+ var scene, camera, renderer, mesh;
 var meshFloor;
 
 var keyboard = {};
-var player = { height: 1.8};
+var player = { height: 1.8, speed: 0.2, turnSpeed:Math.PI*0.02};;
+var USE_WIREFRAME = false;
 
 function init(){
   scene = new THREE.Scene();
@@ -10,14 +11,16 @@ function init(){
 
   mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 0xff4444, wireframe: true})
+    new THREE.MeshBasicMaterial({color: 0xff4444, wireframe:USE_WIREFRAME})
   );
 
+  mesh.position.y += 1; //move the mesh up 1 meter
+  
   scene.add(mesh);
 
   meshFloor = new THREE.Mesh(
     new THREE.PlaneGeometry(10,10, 10,10),
-    new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true})
+    new THREE.MeshBasicMaterial({color: 0xffffff, wireframe:USE_WIREFRAME})
   );
   meshFloor.rotation.x -= Math.PI / 2;
   scene.add(meshFloor);
@@ -40,21 +43,30 @@ function animate(){
   mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.02;
 
-  if(keyboard[87]){// W key
-    camera.position.x += Math.sin(camera.rotation.y) * player.speed;
-    camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
+  if(keyboard[87]){ // W key
+		camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+		camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
+	}
+	if(keyboard[83]){ // S key
+		camera.position.x += Math.sin(camera.rotation.y) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
   }
+  if(keyboard[65]){ // A key
+		camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
+  }
+  if(keyboard[68]){ // D key
+		camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
+	}
 
-  if(keyboard[83]){// S key
-    camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
-    camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
-  }
+  
 
   if (keyboard[37]){ // left arrow key
-    camera.rotation.y += Math.PI * 0.01;
+    camera.rotation.y -= Math.PI * 0.01;
   }
   if (keyboard[39]){ // right arrow key
-    camera.rotation.y -= Math.PI * 0.01;
+    camera.rotation.y += Math.PI * 0.01;
 
   }
 
