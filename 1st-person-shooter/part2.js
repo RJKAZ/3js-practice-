a7ddac5bb67f253e8fb61b252557b130d4515e5e
Richard Kazanowski <rjkaz@comcast.net>
2020-10-23 15:04:50 -0400

    update

--- 1st-person-shooter/part2.js
+++ 1st-person-shooter/part2.js
@@ -1,7 +1,5 @@
 var scene, camera, renderer, mesh;
-var meshFloor, ambientLight, light;
-
-var crate, crateTexture, crateNormalMap, crateBumpMap; 
+var meshFloor;
 
 var keyboard = {};
 var player = { height: 1.8, speed: 0.2, turnSpeed:Math.PI*0.02};;
@@ -13,57 +11,19 @@ function init(){
 
   mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshPhongMaterial({color: 0xff4444, wireframe:USE_WIREFRAME})
+    new THREE.MeshBasicMaterial({color: 0xff4444, wireframe:USE_WIREFRAME})
   );
 
   mesh.position.y += 1; //move the mesh up 1 meter
-  mesh.receiveShadow = true; 
-  mesh.castShadow = true;
   
   scene.add(mesh);
 
   meshFloor = new THREE.Mesh(
     new THREE.PlaneGeometry(10,10, 10,10),
-    new THREE.MeshPhongMaterial({color: 0xffffff, wireframe:USE_WIREFRAME})
+    new THREE.MeshBasicMaterial({color: 0xffffff, wireframe:USE_WIREFRAME})
   );
   meshFloor.rotation.x -= Math.PI / 2;
-  meshFloor.receiveShadow = true;
-  mesh.castShadow= true;
   scene.add(meshFloor);
-
-  ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-  scene.add(ambientLight);
-
-  light = new THREE.PointLight(0xffffff, 0.8, 18);
-  light.position.set(-3, 6, -3);
-  light.castShadow = true; 
-  light.shadow.camera.near = 0.1;
-  light.shadow.camera.far = 25;
-  scene.add(light);
-
-  // lets load the textures 
-
-  var textureLoader = new THREE.TextureLoader();
-  crateTexture = textureLoader.load("crate0/crate0_diffuse.png");
-  crateBumpMap = textureLoader.load("crate0/crate0_bump.png");
-  crateNormalMap = textureLoader.load("crate0/crate0_normal.png");
-
-  // create mesh with the textures
-
-  crate = new THREE.Mesh(
-    new THREE.BoxGeometry(3,3,3),
-    new THREE.MeshPhongMaterial({
-      color:0xffffff,
-      map: crateTexture,
-      bumpMap: crateBumpMap,
-      normalMap:crateNormalMap
-
-    })
-  );
-  scene.add(crate);
-  crate.position.set(2.5, 3/2, 2.5);
-  crate.receiveShadow = true;
-  crate.castShadow = true;
 
 
   camera.position.set(0, player.height,-5);
@@ -71,10 +31,6 @@ function init(){
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(1280, 720);
-
-  renderer.shadowMap.enabled = true; 
-  renderer.shadowMap.type = THREE.BasicShadowMap;
-
   document.body.appendChild(renderer.domElement);
 
   animate();
@@ -86,7 +42,6 @@ function animate(){
 
   mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.02;
-  crate.rotation.y += 0.01;
 
   if(keyboard[87]){ // W key
 		camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
--- 1st-person-shooter/part3.js
+++ 1st-person-shooter/part3.js
@@ -1,8 +1,6 @@
 var scene, camera, renderer, mesh;
 var meshFloor, ambientLight, light;
 
-var crate, crateTexture, crateNormalMap, crateBumpMap; 
-
 var keyboard = {};
 var player = { height: 1.8, speed: 0.2, turnSpeed:Math.PI*0.02};;
 var USE_WIREFRAME = false;
@@ -41,30 +39,6 @@ function init(){
   light.shadow.camera.far = 25;
   scene.add(light);
 
-  // lets load the textures 
-
-  var textureLoader = new THREE.TextureLoader();
-  crateTexture = textureLoader.load("crate0/crate0_diffuse.png");
-  crateBumpMap = textureLoader.load("crate0/crate0_bump.png");
-  crateNormalMap = textureLoader.load("crate0/crate0_normal.png");
-
-  // create mesh with the textures
-
-  crate = new THREE.Mesh(
-    new THREE.BoxGeometry(3,3,3),
-    new THREE.MeshPhongMaterial({
-      color:0xffffff,
-      map: crateTexture,
-      bumpMap: crateBumpMap,
-      normalMap:crateNormalMap
-
-    })
-  );
-  scene.add(crate);
-  crate.position.set(2.5, 3/2, 2.5);
-  crate.receiveShadow = true;
-  crate.castShadow = true;
-
 
   camera.position.set(0, player.height,-5);
   camera.lookAt(new THREE.Vector3(0,player.height,0));
@@ -86,7 +60,6 @@ function animate(){
 
   mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.02;
-  crate.rotation.y += 0.01;
 
   if(keyboard[87]){ // W key
 		camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
